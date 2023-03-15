ab64975b4e70157b3b62a6d0aa0e608c8d8926e8
Sagnik Sengupta (Innleid) <Sagnik.Sengupta@vkm.no>
2023-03-15 14:25:32 +0100

    Trade networks: -Unique colour for the node representing Norway. -Possible to drag the nodes around and export as png, jpeg or pdf.

--- R/Plot_python_tradespecieswise-no-yellow.R
+++ R/Plot_python_tradespecieswise-no-yellow.R
@@ -12,31 +12,31 @@
 
 # Add a date range option
 # Add in option for all trades
-plot_python_trade_network_allsp<-function(importer="NO",min_year=2009){
-  
+plot_python_trade_network_c<-function(Taxon, importer="NO",min_year=2009){
   dat=vkmCites::Pythons |>
     dplyr::mutate(Term2=dplyr::case_when(
       Term=="live"~"live",
       Term=="eggs (live)"~"live",
       TRUE~"parts or product"
     ))|>
+    dplyr::filter(Taxon=={{Taxon}}) |>
     dplyr::filter(Importer=={{importer}}) |>
     dplyr::mutate(Term=Term2) |>
     dplyr::filter(Year>{{min_year}}) |>
     dplyr::group_by(Year, Term, Origin, Exporter,Importer) |>
     dplyr::tally()
   if(dim(dat)[1]<1) stop("Not enough data for your chosen taxon")
-
-
+  
+  
   type = dplyr::tibble("Type"=c(dat$Term, dat$Term))
   source = dplyr::tibble("Source"=c(dat$Origin,dat$Exporter))
   target = dplyr::tibble("Target"=c(dat$Exporter,dat$Importer))
   value =  dplyr::tibble("Value"=c(dat$n, dat$n))
-
+  
   node_label1<-unique(dat$Origin)
   node_label2<-unique(dat$Exporter)
   node_label3<-unique(dat$Importer)
-
+  
   node_label<-c(node_label1, node_label2, node_label3)
   node_label<-unique(node_label)
   
@@ -45,35 +45,35 @@ plot_python_trade_network_allsp<-function(importer="NO",min_year=2009){
   nodes<-data.frame("node"=node_label,
                     #color.background=c("darkgreen"))
                     color.background=node_colours)
-
+  
   nodes=nodes |>
     dplyr::mutate(id=row_number()) |>
     dplyr::mutate(label=node)
-
+  
   edges<-data.frame(from=relations$Source, to=relations$Target,
                     type=type$Type, value=value$Value)
-
+  
   edges=edges |>
     dplyr::left_join(nodes,by=c("from"="node")) |>
     dplyr::mutate(from_id=id) |>
     dplyr::select(-id)
-
+  
   edges=edges |>
     dplyr::left_join(nodes,by=c("to"="node")) |>
     dplyr::mutate(to_id=id) |>
     dplyr::select(-id)
-
+  
   edges=edges |>
     dplyr::mutate(from=from_id,
-           to=to_id)
-
+                  to=to_id)
+  
   edges =edges |>
     dplyr::mutate(color=case_when(type=="live"~ "#FF0000",
-                           TRUE~"#00FFFF"))
-
+                                  TRUE~"#00FFFF"))
+  
   edges=edges |>
     dplyr::mutate(width=value)
-
+  
   # edges=edges |>
   #   mutate(label= type)
   ledges <- data.frame(color = c("#FF0000", "#00FFFF"),
@@ -81,10 +81,10 @@ plot_python_trade_network_allsp<-function(importer="NO",min_year=2009){
   visNetwork::visNetwork(nodes, edges)  |>
     visNetwork::visPhysics(enabled = FALSE) |>
     visNetwork::visLegend(addEdges = ledges)
-
-
-
-
+  
+  
+  
+  
 }
 
 
